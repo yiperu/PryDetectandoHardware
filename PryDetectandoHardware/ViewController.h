5263043dacc2783fd3eed05090b027a55e3eb185
Henry AT <yiperu@gmail.com>
2014-07-24 22:22:53 -0400

    Configuracion Inicial del proyecto de reconocimiento de hardware

---
+++ PryDetectandoHardware/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  PryDetectandoHardware
+//
+//  Created by Henry Antonio Ambicho Trujillo on 7/24/14.
+//  Copyright (c) 2014 Apps4s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *btnDetectandoCamara;
+
+- (IBAction)btnDetectCamara:(id)sender;
+
+
+
+
+
+@end
---
+++ PryDetectandoHardware/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  PryDetectandoHardware
+//
+//  Created by Henry Antonio Ambicho Trujillo on 7/24/14.
+//  Copyright (c) 2014 Apps4s. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnDetectCamara:(id)sender {
+    
+    
+}
+@end
